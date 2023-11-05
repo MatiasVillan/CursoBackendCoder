6325a2c2e27a1cf18c98d3175e02f1bf3d80b44a
MatiasVillan <arv_bua_mvillan@hotmail.com>
2023-11-04 22:34:58 -0300

    desafio 7

---
+++ DesafioEntregable7/src/routes/views.router.js
@@ -0,0 +1,38 @@
+import { Router } from 'express';
+import { productManager } from '../managers/ProductsManager.js';
+
+const router = Router();
+
+/* router.get('/home', async (req, res) => {
+    //const products = await productManager.getProducts({});
+    const products = await productManager.findAll();
+
+    res.render('home', { products: products });
+}); */
+
+router.get('/realtime', async (req, res) => {
+    //const products = await productManager.getProducts({});
+    const products = await productManager.findAll();
+    res.render('realTimeProducts', { products: products });
+});
+
+router.get('/chat', (req, res) => {
+    res.render('chat');
+});
+
+router.get('/', (req, res) => {
+    res.render("login");
+});
+
+router.get('/signup', (req, res) => {
+    res.render("signup");
+});
+
+router.get('/home', async (req, res) => {
+    const { name, email } = req.session;
+    const products = await productManager.findAll();
+
+    res.render("home", { email, name, products: products });
+});
+
+export default router;
